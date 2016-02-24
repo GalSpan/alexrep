3c4814cd791e7f75bacea2b8cb193742bb36562d
Alex Ovusu <Alex Ovusu>
2016-02-24 16:11:52 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>T-argos</title>
+        <link rel="stylesheet" href="main.css">
+    </head>
+    
+    <body>
+    
+        <div class="header">
+        </div>>
+        
+    </body>
+</html>
